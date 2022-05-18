812107fca5c1941bba028431d3636cc2c39dca38
Blad <valtfras@gmail.com>
2022-05-19 01:26:10 +0400

    platform movement

--- model/Platform.test.ts
+++ model/Platform.test.ts
@@ -1,20 +1,52 @@
 import Platform from './Platform';
 
 describe('Platform', () => {
-    test('constructor', () => {
-    //Arrange
-    const expectedX: number = 0;
+  test('constructor', () => {
+    // Arrange
+    const expectedX: number = 50;
     const expectedY: number = 0;
-    const expectedLength: number = 100;
+    const expectedLength: number = 10;
     const expectedHeight: number = 5;
 
-    //Act
+    // Act
     const actualPlatform = new Platform(expectedX, expectedY, expectedLength, expectedHeight);
 
-    //Assert
+    // Assert
     expect(actualPlatform.x).toEqual(expectedX);
     expect(actualPlatform.y).toEqual(expectedY);
     expect(actualPlatform.length).toEqual(expectedLength);
     expect(actualPlatform.height).toEqual(expectedHeight);
-    })
+  });
+
+  test('moveLeft', () => {
+    // Arrange
+    const expectedX: number = 50;
+    const expectedY: number = 0;
+    const expectedLength: number = 10;
+    const expectedHeight: number = 5;
+    const expectedNewX: number = 45;
+
+    const platform = new Platform(expectedX, expectedY, expectedLength, expectedHeight);
+    // Act
+    platform.moveLeft();
+
+    // Assert
+    expect(platform.x).toEqual(expectedNewX);
+  });
+
+  test('moveRight', () => {
+    // Arrange
+    const expectedX: number = 50;
+    const expectedY: number = 0;
+    const expectedLength: number = 10;
+    const expectedHeight: number = 5;
+    const expectedNewX: number = 55;
+
+    const platform = new Platform(expectedX, expectedY, expectedLength, expectedHeight);
+    // Act
+    platform.moveRight();
+
+    // Assert
+    expect(platform.x).toEqual(expectedNewX);
+  });
 });
--- model/Platform.ts
+++ model/Platform.ts
@@ -1,19 +1,31 @@
+const ONE_STEP = 5;
+
 export default class Platform {
   x: number;
+
   y: number;
+
   length: number;
+
   height: number;
 
   constructor(
     x: number,
     y: number,
     length: number,
-    height: number
-    ) {
+    height: number,
+  ) {
     this.x = x;
     this.y = y;
     this.length = length;
     this.height = height;
-  
+  }
+
+  moveLeft() {
+    this.x -= ONE_STEP;
+  }
+
+  moveRight() {
+    this.x += ONE_STEP;
   }
 }
